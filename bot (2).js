62762802f7d3d4844a9a6c000c7b2f3d3ba4bfe0
tahargridi <45130792+tahargridi@users.noreply.github.com>
2018-11-25 12:37:53 -0800

    Add files via upload

---
+++ bot (2).js
@@ -0,0 +1,17 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+
+client.on('ready', async() => {
+var server = "511237640092123150"; // ايدي السررفر
+var channel = "511238296840568842";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , **')
+    },305);
+})
+
+client.login(توكن حسابك);
